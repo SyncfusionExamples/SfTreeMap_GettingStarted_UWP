82e77fcf415490b2743a318feb165dfa111434f8
AzureAD\RamyaSoundarRajan <ramya.soundarrajan@syncfusion.com>
2020-06-25 18:24:56 +0530

    Added getting started sample for TreeMap UWP.

---
+++ GettingStarted_Sample/Model/PopulationDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_Sample
+{
+    public class PopulationDetail
+    {
+        public string Continent { get; set; }
+        public string Country { get; set; }
+        public double Growth { get; set; }
+        public double Population { get; set; }
+    }
+}
